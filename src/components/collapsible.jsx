9b5c7e98b4434910666b909d45e087653e16d112
Jayesh <1032190683@mitwpu.edu.in>
2022-07-03 15:01:47 +0530

    fixed layout

--- src/components/collapsible.jsx
+++ src/components/collapsible.jsx
@@ -6,7 +6,7 @@ function Collapsible(props) {
     return (
         <div className="collapsible">
             <div className="header1" {...getToggleProps()}>
-                {isExpanded ? '' : 'Know More'}
+                {isExpanded ? '': 'Know More'}
             </div>
             <div {...getCollapseProps()}>
                 <div className="content1">
@@ -14,7 +14,7 @@ function Collapsible(props) {
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque orci quis convallis molestie. Donec eleifend, nibh in tempus vehicula, magna metus tempor massa, eget ultricies velit lectus ut libero. Fusce iaculis luctus elit, vel tempor mi suscipit ullamcorper. Sed lectus mauris, ultrices id varius et, cursus nec nisl. Morbi sagittis lorem pretium placerat elementum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce pretium, risus eget dictum interdum, ante ipsum fermentum ex, non semper purus erat sed nisi. Vivamus rhoncus mi vitae semper fermentum. Praesent ut massa in dui viverra sagittis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean eu est urna. Cras non elit eget purus accumsan egestas sit amet eu dolor. Phasellus eu dolor ipsum. Curabitur vel leo scelerisque, vestibulum justo a, commodo urna.</p>
                 </div>
                 <div className="header1" {...getToggleProps()}>
-                {!isExpanded ? '' : 'Collapse'}
+                {!isExpanded ? '': 'Collapse'}
                 </div>
             </div>
         </div>
